afa8b75b6e828c00783e1df95d7ae89b8091405c
Labalou <louis.audeon@mail.be>
2014-10-06 22:02:14 +0200

    fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -32,7 +32,7 @@ class ProjectsController < ApplicationController
    @project  = Project.new(project_params)
    # Set User's project
    @project.user = current_user
-   @project.author = @project.user                                                                                                                                                                                     
+   @project.author = @project.user.name                                                                                                                                                                                     
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
