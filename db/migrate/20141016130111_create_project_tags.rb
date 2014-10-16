e551594e499bd69313ae84bd11d97e3ed55c7f98
labalou <audeon.louis@gmail.com>
2014-10-16 16:43:41 +0200

    fix bug migrate

--- db/migrate/20141016130111_create_project_tags.rb
+++ db/migrate/20141016130111_create_project_tags.rb
@@ -1,9 +1,8 @@
 class CreateProjectTags < ActiveRecord::Migration
   def change
-  	#drop_table :table_project_tags
-    # create_table :project_tags do |t|
-    # 	t.belongs_to :tag
-    # 	t.belongs_to :project
-    # end
+    create_table :project_tags do |t|
+    	t.belongs_to :tag
+    	t.belongs_to :project
+    end
   end
 end
